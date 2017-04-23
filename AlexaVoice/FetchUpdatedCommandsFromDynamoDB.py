24c0a137b1810c857944bc31179e39bd0d553856
vibhormishra <vibhor.mishra@colorado.edu>
2017-04-23 11:24:08 -0600

    Adding Alexa Code
    This includes Alexa Lambda script and Python local code to fetch the
    updated bot movement commands.

---
+++ AlexaVoice/FetchUpdatedCommandsFromDynamoDB.py
@@ -0,0 +1,55 @@
+import boto3
+import json
+import decimal
+from boto3.dynamodb.conditions import Key, Attr
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+import warnings
+
+
+
+
+def get_move():
+    moveCommand = ""
+    #dynamodb = boto3.resource('dynamodb', region_name='us-east-1', aws_access_key_id='AKIAJMHJ2TULWSOIOMVQ', aws_secret_access_key='yYUv1YPFZbCF7Wej8ZcC')
+    #dynamodb = boto3.client('dynamodb', region_name='us-east-1', aws_access_key_id='AKIAJMHJ2TULWSOIOMVQ', aws_secret_access_key='yYUv1YPFZbCF7Wej8ZcC')
+    
+    session = boto3.Session(
+            aws_access_key_id='AKIAJMHJ2TULWSOIOMVQ',
+            aws_secret_access_key='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',
+            region_name='us-east-1',
+        )
+    dynamodb = session.resource('dynamodb')
+    table = dynamodb.Table('RobotCommands')
+    
+    #print(list(dynamodb.tables.all()))
+#     newMove = "Move";
+#     response = table.update_item(
+#         Key={
+#             'ID':1
+#             },
+#         UpdateExpression="set Command = :c",
+#         ExpressionAttributeValues={
+#             ':c': newMove
+#         },
+#         ReturnValues="UPDATED_NEW"
+#     )
+    
+    #print(json.dumps(response, indent=4, cls=DecimalEncoder))    
+    last_move = "";
+    while(1):        
+        response = table.scan()
+        moveCommand = response['Items'][0]['Command']    
+        if last_move != moveCommand:
+            print moveCommand;
+            last_move = moveCommand; 
+        if moveCommand == "stop":
+            break;
+        
+    #print response;
+    
+    
+    #return moveCommand
+
+get_move();
+print "Stopped. Exiting."
---
+++ AlexaVoice/LambdaScript.py
@@ -0,0 +1,214 @@
+import boto3
+import json
+import decimal
+from boto3.dynamodb.conditions import Key, Attr
+
+def build_speechlet_response(title, output, reprompt_text, should_end_session):
+    return {
+        'outputSpeech': {
+            'type': 'PlainText',
+            'text': output
+        },
+        'card': {
+            'type': 'Simple',
+            'title': "SessionSpeechlet - " + title,
+            'content': "SessionSpeechlet - " + output
+        },
+        'reprompt': {
+            'outputSpeech': {
+                'type': 'PlainText',
+                'text': reprompt_text
+            }
+        },
+        'shouldEndSession': should_end_session
+    }
+
+
+def build_response(session_attributes, speechlet_response):
+    return {
+        'version': '1.0',
+        'sessionAttributes': session_attributes,
+        'response': speechlet_response
+    }
+
+
+# --------------- Functions that control the skill's behavior ------------------
+
+def get_welcome_response():
+    """ If we wanted to initialize the session to have some attributes we could
+    add those here
+    """
+
+    session_attributes = {}
+    card_title = "Welcome"
+    speech_output = "please tell me the direction of the robot"
+    # If the user either does not reply to the welcome message or says something
+    # that is not understood, they will be prompted again with this text.
+    reprompt_text = "please tell me the direction of the robot"
+    should_end_session = False
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+
+def handle_session_end_request():
+    card_title = "Session Ended"
+    speech_output = "Thank you for trying the Alexa Skills Kit sample. " \
+                    "Have a nice day! "
+    # Setting this to true ends the session and exits the skill.
+    should_end_session = True
+    return build_response({}, build_speechlet_response(
+        card_title, speech_output, None, should_end_session))
+
+
+def create_bot_next_move_attribute(bot_next_move):
+    return {"nextBotCommand": bot_next_move}
+
+
+def set_color_in_session(intent, session):
+    """ Sets the Command in the session and prepares the speech to reply to the
+    user.
+    """
+
+    card_title = intent['name']
+    session_attributes = {}
+    should_end_session = False
+
+    if 'Command' in intent['slots']:
+        bot_next_move = intent['slots']['Command']['value']
+        session_attributes = create_bot_next_move_attribute(bot_next_move)
+        speech_output = "I now know the direction of the bot is" + \
+                        bot_next_move 
+        
+        updateMove(bot_next_move);                
+        reprompt_text = "You can ask me you direction of the bot, " 
+                        
+    else:
+        speech_output = "Please try again."
+        reprompt_text = "try again bro"
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+
+def get_color_from_session(intent, session):
+    session_attributes = {}
+    reprompt_text = None
+
+    if session.get('attributes', {}) and "nextBotCommand" in session.get('attributes', {}):
+        bot_next_move = session['attributes']['nextBotCommand']
+        speech_output = "Your direction is " + bot_next_move + \
+                        ". Goodbye."
+        should_end_session = True
+        updateMove(bot_next_move);
+    else:
+        speech_output = "I'm not sure what your bot's next move Command is. " \
+                        "You can say, the direction is."
+        should_end_session = False
+
+    # Setting reprompt_text to None signifies that we do not want to reprompt
+    # the user. If the user does not respond or says something that is not
+    # understood, the session will end.
+    return build_response(session_attributes, build_speechlet_response(
+        intent['name'], speech_output, reprompt_text, should_end_session))
+
+
+# --------------- Events ------------------
+
+def on_session_started(session_started_request, session):
+    """ Called when the session starts """
+
+    print("on_session_started requestId=" + session_started_request['requestId']
+          + ", sessionId=" + session['sessionId'])
+
+
+def on_launch(launch_request, session):
+    """ Called when the user launches the skill without specifying what they
+    want
+    """
+
+    print("on_launch requestId=" + launch_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+    # Dispatch to your skill's launch
+    return get_welcome_response()
+
+
+def on_intent(intent_request, session):
+    """ Called when the user specifies an intent for this skill """
+
+    print("on_intent requestId=" + intent_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+
+    intent = intent_request['intent']
+    intent_name = intent_request['intent']['name']
+
+    # Dispatch to your skill's intent handlers
+    if intent_name == "MyColorIsIntent":
+        return set_color_in_session(intent, session)
+    elif intent_name == "WhatsMyColorIntent":
+        return get_color_from_session(intent, session)
+    elif intent_name == "AMAZON.HelpIntent":
+        return get_welcome_response()
+    elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
+        return handle_session_end_request()
+    else:
+        raise ValueError("Invalid intent")
+
+
+def on_session_ended(session_ended_request, session):
+    """ Called when the user ends the session.
+
+    Is not called when the skill returns should_end_session=true
+    """
+    print("on_session_ended requestId=" + session_ended_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+    # add cleanup logic here
+
+
+# --------------- Main handler ------------------
+
+def lambda_handler(event, context):
+    """ Route the incoming request based on type (LaunchRequest, IntentRequest,
+    etc.) The JSON body of the request is provided in the event parameter.
+    """
+    print("event.session.application.applicationId=" +
+          event['session']['application']['applicationId'])
+
+    """
+    Uncomment this if statement and populate with your skill's application ID to
+    prevent someone else from configuring a skill that sends requests to this
+    function.
+    """
+    # if (event['session']['application']['applicationId'] !=
+    #         "amzn1.echo-sdk-ams.app.[unique-value-here]"):
+    #     raise ValueError("Invalid Application ID")
+
+    if event['session']['new']:
+        on_session_started({'requestId': event['request']['requestId']},
+                           event['session'])
+
+    if event['request']['type'] == "LaunchRequest":
+        return on_launch(event['request'], event['session'])
+    elif event['request']['type'] == "IntentRequest":
+        return on_intent(event['request'], event['session'])
+    elif event['request']['type'] == "SessionEndedRequest":
+        return on_session_ended(event['request'], event['session'])
+
+
+def updateMove(newMove):
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+    table = dynamodb.Table('RobotCommands')
+    
+    
+    response = table.update_item(
+        Key={
+            'ID':1
+            },
+        UpdateExpression="set Command = :c",
+        ExpressionAttributeValues={
+            ':c': newMove
+        },
+        ReturnValues="UPDATED_NEW"
+    )
+    
+    #print(json.dumps(response, indent=4, cls=DecimalEncoder))
+    
+
